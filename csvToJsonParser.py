14afd614af3cf56a32bae74adf124834f8cbf52b
ulughbekula <ulughbekula@gmail.com>
2022-10-29 20:26:49 +0900

    Added patterns 22 to 25

--- csvToJsonParser.py
+++ csvToJsonParser.py
@@ -117,8 +117,8 @@ def make_json(csvPath, jsonFilePath, grade, subject, language):
                         }
                     }
 
-                    # Patterns 1,2,7,11,12,13,18,17,19,20,21
-                    if row['pattern'] == "1" or row['pattern'] == "2" or row['pattern'] == "7" or row['pattern'] == "11" or row['pattern'] == "12" or row['pattern'] == "13" or row['pattern'] == "17" or row['pattern'] == "18" or row['pattern'] == "19" or row['pattern'] == "20" or row['pattern'] == "21":
+                    # Patterns 1,2,7,11,12,13,18,17,19,20,21,23,25
+                    if row['pattern'] == "1" or row['pattern'] == "2" or row['pattern'] == "7" or row['pattern'] == "11" or row['pattern'] == "12" or row['pattern'] == "13" or row['pattern'] == "17" or row['pattern'] == "18" or row['pattern'] == "19" or row['pattern'] == "20" or row['pattern'] == "21" or row['pattern'] == "23" or row['pattern'] == '25':
                         # Parse Options
                         question['question']['options'] = parse_options(row['id'], row['options'])
                     elif row['pattern'] == "3" or row['pattern'] == "5":
@@ -152,8 +152,8 @@ def make_json(csvPath, jsonFilePath, grade, subject, language):
                                     'sign': line[1],
                                     'right': line[2]
                                 })
-                    # Patterns 6,14,15,16
-                    elif row['pattern'] == "6" or row['pattern'] == "14" or row['pattern'] == "15" or row['pattern'] == '16':                     
+                    # Patterns 6,14,15,16,22,24
+                    elif row['pattern'] == "6" or row['pattern'] == "14" or row['pattern'] == "15" or row['pattern'] == '16' or row['pattern'] == '22' or row['pattern'] == '24':                     
                         question['question']['problem'] = parse_options(row['id'], row['problem'])
                         question['question']['solution'] = parse_options(row['id'], row['solution'])
                     elif row['pattern'] == "8":
